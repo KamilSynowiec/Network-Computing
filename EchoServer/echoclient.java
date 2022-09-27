77e1dd778f84bab687c1ef22aab73fd5bdc00469
Kamil Synowiec <1935002@brunel.ac.uk>
2022-09-27 13:43:31 +0100

    Update echoclient.java

--- EchoServer/echoclient.java
+++ EchoServer/echoclient.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import ajva.net.*;
 
+public class echoclient
+{
+  public static void main(String[]args) throws IOException
+  {
+    //set up the socket, in and out variables
+    Socket echoSocket=null;
+    PrintWriter echoClientOutput=null;
+    BufferedWriter echoClientInput=null;
+    
+    try
+    {
+      echoSocket=new Socket("localhost", 4321);
+      
+      echoClientOutput=new PrintWriter(echoSocket.getOutputStream(),true);
+      echoClientInput=new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+     
+    }
+    catch(UnknownHostException e)
+    {
+      System.err.println("Couldn't get I/O for the connection to: 4321");
+      System.exit(1);
+    }
+  }
+}
